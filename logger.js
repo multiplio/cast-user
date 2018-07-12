5c118988673e6e0cb7fd697682d2d0266224267a
WhoMeNope <whomenope@outlook.com>
2018-07-12 21:17:37 +0200

    logger for docker

--- logger.js
+++ logger.js
@@ -4,9 +4,6 @@ const { combine, timestamp, label, printf } = format;
 const logFormat = printf(info => {
   return `${info.timestamp}:${info.level}: ${info.message}`;
 });
-const consoleFormat = printf(info => {
-  return `${info.level}: ${info.message}`;
-});
 
 const logger = createLogger({
   level: 'info',
@@ -15,19 +12,21 @@ const logger = createLogger({
     logFormat
   ),
   transports: [
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'combined.log' })
+    new transports.Console({ format: logFormat })
   ]
 });
+
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
-    new transports.Console({
-      format: consoleFormat
-    }));
+    // - Write all logs error (and below) to `error.log`.
+    new transports.File({ filename: 'error.log', level: 'error' }),
+  );
+  logger.add(
+    // - Write to all logs with level `info` and below to `combined.log`
+    new transports.File({ filename: 'combined.log' })
+  );
 }
 
 module.exports = logger;

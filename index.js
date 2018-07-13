ca133dcab6d4d0a0c7402b3b79f413242af78b49
WhoMeNope <whomenope@outlook.com>
2018-07-13 17:03:09 +0200

    twitter login database persist

--- index.js
+++ index.js
@@ -21,26 +21,34 @@ app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: process.env.COOKIE_SECRET, resave: true, saveUninitialized: true }));
 
-// setup twitter auth
-require('./twitter.js')(app);
+// setup users database
+require('./user.js')
+  .then(function(User) {
+    // setup twitter auth
+    require('./twitter.js')(app, User);
 
-// DEBUG routes
-if (process.env.NODE_ENV == 'development') {
-  app.get(
-    '/login',
-    function(req, res) {
-      res.send('login')
-    });
+    // DEBUG routes
+    if (process.env.NODE_ENV !== 'production') {
+      app.get(
+        '/login',
+        function(req, res) {
+          res.send('login')
+        });
 
-  app.get(
-    '/',
-    function(req, res) {
-      res.send(req.user);
-    });
+      app.get(
+        '/',
+        function(req, res) {
+          res.send(req.user);
+        });
 
-  logger.log('debug', 'setup debug routes')
-}
+      logger.debug('setup debug routes');
+    }
 
-// start server
-app.listen(process.env.PORT);
-logger.info(`listening at localhost:${process.env.PORT}`);
+    // start server
+    app.listen(process.env.PORT);
+    logger.info(`listening at localhost:${process.env.PORT}`);
+  })
+  .catch(function(err) {
+    logger.error(err);
+    process.exit(0);
+  });
--- twitter.js
+++ twitter.js
@@ -1,53 +1,46 @@
 const logger = require('./logger.js');
+const util = require('util');
 
 const passport = require('passport');
 const TwitterStrategy = require('passport-twitter').Strategy;
 
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Twitter profile is serialized
-// and deserialized.
-passport.serializeUser(
-  function(user, cb) {
-    logger.log('debug', `serializing: ${user}`);
-    cb(null, user);
-  });
-
-passport.deserializeUser(
-  function(obj, cb) {
-    logger.log('debug', `deserializing: ${obj}`);
-    cb(null, obj);
-  });
-
-passport.use(
-  new TwitterStrategy({
-    consumerKey: process.env.TWITTER_CONSUMER_KEY,
-    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: `http://localhost:${process.env.PORT}/auth/twitter/callback`
-  },
-    function(token, tokenSecret, profile, cb) {
-      // In this example, the user's Twitter profile is supplied as the user
-      // record.  In a production-quality application, the Twitter profile should
-      // be associated with a user record in the application's database, which
-      // allows for account linking and authentication with other identity
-      // providers.
-      logger.log('debug', `authenticating: ${profile.id}`);
-      return cb(null, profile);
-
-      // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-      //   return cb(err, user);
-      // });
-    }
-  ));
-logger.info('inited twitter passport');
-
 // setup auth handling
-const setup = function(app) {
+const setup = function(app, User) {
+  // Configure Passport authenticated session persistence.
+  passport.serializeUser(
+    function(user, cb) {
+      logger.debug(`serializing: ${user.displayName}`);
+      cb(null, user._id);
+    });
+
+  passport.deserializeUser(
+    function(id, cb) {
+      logger.debug(`deserializing: ${id}`);
+      User.findById(id, cb);
+    });
+
+  passport.use(
+    new TwitterStrategy({
+      consumerKey: process.env.TWITTER_CONSUMER_KEY,
+      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+      callbackURL: process.env.TWITTER_CALLBACK_URL
+    },
+      function(token, tokenSecret, profile, cb) {
+        logger.log('debug', `authenticating: ${profile.id}`);
+
+        var user = {
+          displayName: profile.displayName,
+          profileImageUrl: profile._json.profile_image_url_https,
+          twitterId: profile.id,
+          twitterAccessLevel: profile._accessLevel
+        };
+        User.findOrCreate(user, function (err, user) {
+          return cb(err, user);
+        });
+      }
+    ));
+  logger.info('inited twitter passport');
+
   // initialize passport and restore authentication state, if any, from the session.
   app.use(passport.initialize());
   app.use(passport.session());
@@ -65,7 +58,7 @@ const setup = function(app) {
     ),
     function(req, res) {
       // Successful authentication, redirect home.
-      logger.log('debug', `succesful authentication for: ${req.user}`)
+      logger.debug(`succesful authentication for: ${req.user.displayName}`)
       res.redirect('/');
     });
 

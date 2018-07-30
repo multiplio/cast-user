186ed1d503d58756f8c39423f6f239a857ae65f8
WhoMeNope <whomenope@outlook.com>
2018-07-30 13:18:17 +0200

    use new url parser

--- db.js
+++ db.js
@@ -8,7 +8,8 @@ module.exports = function (url, name) {
     poolSize: 10, // Maintain up to 10 socket connections
     connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
+    family: 4, // Use IPv4, skip trying IPv6
+    useNewUrlParser: true
   };
 
   return new Promise(function (resolve, reject) {

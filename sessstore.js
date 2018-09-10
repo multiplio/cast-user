5c6a20cdeb0fb949779e30ad7d320e816ce5fd2e
WhoMeNope <whomenope@outlook.com>
2018-09-10 12:19:09 +0200

    use same db for users and sessions

--- sessstore.js
+++ sessstore.js
@@ -1,7 +1,7 @@
 const StoreIniter = require('connect-mongodb-session');
 const logger = require('./logger.js');
 
-const uri = `mongodb://${process.env.SESSION_DB_USER}:${process.env.SESSION_DB_PASSWORD}@${process.env.SESSION_DB_ADDRESS}/${process.env.SESSION_DB_NAME}`;
+const uri = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_ADDRESS}/${process.env.DATABASE_NAME}`;
 
 module.exports = function (session) {
   return new Promise(function (resolve, reject) {
@@ -14,13 +14,13 @@ module.exports = function (session) {
       });
 
       store.on('connected', function() {
-        store.client; // The underlying MongoClient object from the MongoDB driver
-        logger.info(`got connection to session store ${process.env.SESSION_DB_NAME}`);
+        // store.client; // The underlying MongoClient object from the MongoDB driver
+        logger.info(`got connection to session store ${process.env.DATABASE_NAME}`);
         resolve(store);
       });
 
       store.on('error', function(err) {
-        logger.error(`${err} : retrying for session store`);
+        logger.error(`${err} : retrying for session store ${process.env.DATABASE_NAME}`);
         setTimeout(init, 2000);
       });
     })();
